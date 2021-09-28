2a19582e3258f882a3cc369f40ca18c35775ecbc
xslingcn <xslings@163.com>
2021-09-29 00:41:44 +0800

    Style: change theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'turna-100': "#74D7D0",
+        'turna-200': "#60D1C9",
+        'turna-300': '#4CCBC2',
+        'turna-400': "#39C5BB",
+        'turna-500': '#33B1A8',
+        'turna-600': '#2E9D95',
+        'turna-700': '#288A83',
+
+      }
+    },
   },
   variants: {
     extend: {},
