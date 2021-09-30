feda746c21cb21fb49600ef0270ff1f9901d83c7
xslingcn <xslings@163.com>
2021-10-01 03:16:40 +0800

    Dev: make live 3d model

--- src/pages/Hero.vue
+++ src/pages/Hero.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="container flex flex-col-reverse md:flex-row items-center gap-12 mt-14 md:mt-28 md:ml-44">
+  <div class="container flex flex-col-reverse md:flex-row items-center gap:6 md:gap-12 mt-14 md:mt-28 md:ml-44">
+    <!-- continue arrow -->
+    <transition name="continue" class="animate__animated animate__faster" enter-active-class="animate__fadeInUp" :duration="1000">
+      <svg v-if="isSubtitleComplete" class="w-6 h-6 mt-8 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+      </svg>
+    </transition>
     <!-- content -->
     <div class="flex flex-1 flex-col items-center md:items-start">
       <h2 class="typing text-gray-700 text-4xl text-center mb-8 md:text-6xl lg:text-8xl md:text-left"></h2>
-      <transition name="subTitle" class="animate__animated animate__faster" enter-active-class="animate__fadeIn" :duration="{ enter: 1000, leave: 300 }">
-        <p v-if="isComplete" class="text-gray-900 mt-9 md:mt-11 text-2xl md:text-4xl lg:text-6xl text-center md:text-left md:px-2 lg:px-6 mb-6">
+      <transition name="subTitle" class="animate__animated animate__faster" enter-active-class="animate__fadeIn" :duration="1000" v-on:after-enter="isSubtitleComplete = !isSubtitleComplete">
+        <p v-if="isTypingComplete" class="text-gray-900 mt-9 md:mt-11 text-2xl md:text-4xl lg:text-6xl text-center md:text-left md:px-2 lg:px-6 mb-6">
           Craft <br />
           the Universe
         </p>
       </transition>
-
       <div class="flex justify-center flex-wrap gap-6"></div>
+    </div>
+    <!-- 3D model -->
+    <div class="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0">
+      <canvas id="three" class="hidden md:block md:w-full md:h-full"></canvas>
+      <img src="src/assets/img/turna-hero-right-transparent-mobile.webp" class="md:hidden w-2/3 h-2/3" />
     </div>
   </div>
 </template>
@@ -18,12 +28,17 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import TypeIt from "typeit";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+import Create3D from "../ts/3dhandler";
 
 export default defineComponent({
   setup() {},
   data() {
     return {
-      isComplete: false,
+      isTypingComplete: false,
+      isSubtitleComplete: false,
     };
   },
   mounted() {
@@ -33,9 +48,61 @@ export default defineComponent({
       speed: 90,
       lifeLike: true,
       afterComplete: async (instance: TypeIt) => {
-        this.isComplete = true;
+        this.isTypingComplete = true;
       },
     }).go();
+    this.initThree();
+  },
+  methods: {
+    afterEnter() {},
+    initThree() {
+      const canvas = document.querySelector("#three");
+      if (!canvas) throw "Element not Found!";
+      const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+      const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+      const scene = new THREE.Scene();
+      scene.background = new THREE.Color(0xe5e7eb);
+      const control = new OrbitControls(camera, renderer.domElement);
+
+      camera.position.x = 0;
+      camera.position.y = 4.5;
+      camera.position.z = 5;
+      camera.zoom = 1.5;
+
+      const TurnA = new Create3D(scene, renderer, camera, control);
+
+      TurnA.load("src/assets/scene.json");
+      TurnA.setControl();
+
+      window.addEventListener("resize", TurnA.onWindowResize);
+      TurnA.createComposer();
+
+      function checkSize() {
+        let breakPoints: number[] = [640, 768, 1024, 1920];
+        breakPoints.forEach((width) => {
+          if (window.matchMedia(`(max-width: ${width}px)`).matches) {
+            switch (width) {
+              case 640:
+                return [640, 480];
+              case 768:
+                return [768, 640];
+              case 1024:
+                return [1024, 1024];
+              case 1920:
+                return [1024, 1024];
+            }
+          }
+        });
+      }
+
+      function animate() {
+        control.update();
+        TurnA.onWindowResize();
+        requestAnimationFrame(animate);
+        TurnA.composer.render();
+      }
+      animate();
+    },
   },
 });
 </script>
---
+++ src/ts/3dhandler.ts
@@ -0,0 +1,197 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { EffectComposer, Pass } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import { BleachBypassShader } from 'three/examples/jsm/shaders/BleachBypassShader.js';
+import { ColorCorrectionShader } from 'three/examples/jsm/shaders/ColorCorrectionShader.js';
+import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader.js';
+import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader.js';
+
+export default class Create3D  {
+
+    composer: EffectComposer
+    effectFXAA: Pass
+    scene: THREE.Scene
+    renderer: THREE.WebGLRenderer
+    camera: THREE.PerspectiveCamera
+    control: OrbitControls
+
+    constructor(scene: THREE.Scene,renderer: THREE.WebGLRenderer,camera: THREE.PerspectiveCamera,control:OrbitControls) {
+        this.scene = scene;
+        this.renderer = renderer
+        this.camera = camera;
+        this.control = control;
+        this.composer = new EffectComposer(this.renderer);
+        this.effectFXAA = new ShaderPass(FXAAShader);
+    }
+
+
+    setControl() {
+        this.control.minDistance = 0;
+        this.control.maxDistance = 200;
+        this.control.enableDamping = true;
+    }
+
+    generateMaterial(lightPosition: THREE.Vector3, lightColor: String, lightIntensity: String, translucencyColor: String) {
+        
+        var Uniforms = {
+            u_lightPos: {
+              type: 'v3',
+              value: lightPosition,
+            },
+        };
+        
+      
+          var Material = new THREE.ShaderMaterial({
+            uniforms: Uniforms,
+            fragmentShader: `
+              varying vec3 v_fragmentPos;
+              varying vec3 v_normal;
+              uniform vec3 u_lightPos;
+              void main(void)
+              {
+                  vec3 _LightColor0 = vec3(${ lightColor });;  
+                  float _LightIntensity0 = ${ lightIntensity };
+                  vec3 translucencyColor = vec3(${translucencyColor });;
+                  vec3 toLightVector = u_lightPos - v_fragmentPos;
+                  float lightDistanceSQ = dot(toLightVector, toLightVector);
+                  vec3 lightDir = normalize(toLightVector);
+                  float ndotl = max(0.0, dot(v_normal, lightDir));
+                  float inversendotl = step(0.0, dot(v_normal, -lightDir));
+                  vec3 lightColor = _LightColor0.rgb * ndotl / lightDistanceSQ * _LightIntensity0; 
+                  vec3 subsurfacecolor = translucencyColor.rgb * inversendotl / lightDistanceSQ * _LightIntensity0;
+                  vec3 final = subsurfacecolor + lightColor;
+                  gl_FragColor=vec4(final,1.0);
+              }`,
+            vertexShader: `
+              varying vec3 v_fragmentPos;
+              varying vec3 v_normal;
+                          
+              void main()
+              {
+                  vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+                  v_fragmentPos =  (modelMatrix * vec4( position, 1.0 )).xyz;
+                  v_normal =  (modelMatrix * vec4( normal, 0.0 )).xyz;
+                  gl_Position = projectionMatrix * mvPosition;
+              }
+            `
+          });
+          return Material;
+    }
+
+    load(path: string){
+        const loader = new THREE.ObjectLoader();
+        return loader.load(
+            path,
+            (obj) => {               
+            var round1 = obj.getObjectByName("001");
+            if (round1 instanceof THREE.Mesh) {
+                var lightPosition = new THREE.Vector3(-0.6, 1.5, 0.60);
+                    
+                round1.material = this.generateMaterial(
+                    lightPosition,
+                    '0.25,0.88,0.50',
+                    '1.2',
+                    '0.11,0.80,0.44' 
+                );
+                }
+                
+                var round2 = obj.getObjectByName("004");
+                if (round2 instanceof THREE.Mesh) {
+                    var lightPosition = new THREE.Vector3(-0.6, 1.5, 0.60);
+                    round2.material = this.generateMaterial(
+                        lightPosition,
+                        '0.25,0.88,0.50',
+                        '1.2',
+                        '0.11,0.80,0.44' 
+                    );
+                }
+                var folder = obj.getObjectByName("002");
+                if (folder instanceof THREE.Mesh) {
+                    var lightPosition = new THREE.Vector3(0.8, 1.15, 0.60);
+                    folder.material = this.generateMaterial(
+                        lightPosition,
+                        '1.00,0.37,0.09',
+                        '1.2',
+                        '1.00,0.37,0.09' 
+                    );
+                }
+
+                var paper = obj.getObjectByName("005");
+                if (paper instanceof THREE.Mesh) {
+                    var lightPosition = new THREE.Vector3(0.8, 1.15, 0.60);
+                    paper.material = this.generateMaterial(
+                        lightPosition,
+                        '1.00,1.00,1.00',
+                        '1.2',
+                        '1.00,1.00,1.00' 
+                    );
+                }
+                
+                var cube = obj.getObjectByName("003");
+                if (cube instanceof THREE.Mesh) {
+                    var lightPosition = new THREE.Vector3(0.4, 0.4, 0.2);
+                        
+                    cube.material = this.generateMaterial(
+                        lightPosition,
+                        '1.0, 0.5, 0.5',
+                        '1.5',
+                        '1.0,0.47,0.46' 
+                    );
+                }
+                
+                this.scene.add(obj);
+            },
+          (err) => {
+          }
+        );
+    }
+
+    createComposer() {
+        this.renderer.autoClear = false;
+
+        const renderModel = new RenderPass(this.scene, this.camera);
+        const effectBleach = new ShaderPass(BleachBypassShader);
+        const effectColor = new ShaderPass(ColorCorrectionShader);
+        const gammaCorrection = new ShaderPass(GammaCorrectionShader);
+
+        this.effectFXAA.uniforms["resolution"].value.set(1 / window.innerWidth, 1 / window.innerHeight);
+
+        effectBleach.uniforms["opacity"].value = 0.2;
+        effectColor.uniforms["powRGB"].value.set(1.4, 1.45, 1.45);
+        effectColor.uniforms["mulRGB"].value.set(1.1, 1.1, 1.1);
+
+        this.composer.addPass(renderModel);
+        this.composer.addPass(this.effectFXAA);
+        this.composer.addPass(effectBleach);
+        this.composer.addPass(effectColor);
+        this.composer.addPass(gammaCorrection);
+    }
+    
+    onWindowResize() {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();
+
+        this.renderer.setSize(width, height);
+        this.composer.setSize(width, height);
+
+        this.effectFXAA.uniforms.resolution.value.set(1 / width, 1 / height);
+    }
+
+    responsiveResize(size:number[]){
+
+        this.camera.aspect = size[0] / size[1];
+        this.camera.updateProjectionMatrix();
+
+        this.renderer.setSize(size[0], size[1]);
+        this.composer.setSize(size[0], size[1]);
+
+        // this.effectFXAA.uniforms.resolution.value.set(1 / size[0], 1 / size[1]);
+    }
+
+}
