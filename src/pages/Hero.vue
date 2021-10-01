0cefb4af3cd6450c83a961c555f45c0495e77b0d
xslingcn <xslings@163.com>
2021-10-02 00:35:25 +0800

    Dev: make title swifting animation

--- src/pages/Hero.vue
+++ src/pages/Hero.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="container flex flex-col-reverse md:flex-row items-center gap:6 md:gap-12 mx-auto mt-14 md:mt-28">
     <!-- content -->
-    <div class="flex flex-1 flex-col items-center md:items-center">
-      <h2 class="typing text-gray-700 text-4xl text-center mb-8 md:text-6xl lg:text-8xl md:text-left"></h2>
-      <transition name="subTitle" class="animate__animated animate__faster" enter-active-class="animate__fadeIn" :duration="1000" v-on:after-enter="isSubtitleComplete = !isSubtitleComplete">
-        <p v-if="isTypingComplete" class="text-gray-900 mt-9 md:mt-11 text-2xl md:text-4xl lg:text-6xl text-center md:text-left md:px-2 lg:px-6 mb-6">
-          Craft <br />
-          the Universe
-        </p>
-      </transition>
+    <div class="flex flex-1 flex-col item-center md:items-start">
+      <h2 class="text-gray-700 text-center md:text-left text-4xl xl:text-8xl">
+        Greetings from
+        <div class="typing relative h-10 xl:h-24 overflow-hidden xl:mt-2.5">
+          <span data-show>∀Live</span>
+          <span>∀Rchive</span>
+          <span>∀Community</span>
+        </div>
+      </h2>
+      <h3 class="text-gray-600 mt-9 md:mt-11 text-2xl xl:text-6xl text-center md:text-left md:pl-2 animate__animated animate__fadeIn">
+        Craft <br />
+        Your Universe
+      </h3>
     </div>
     <!-- 3D model -->
     <div class="flex justify-center flex-1">
@@ -18,7 +23,7 @@
   </div>
   <!-- continue arrow -->
   <transition name="continue" class="animate__animated animate__faster" enter-active-class="animate__fadeInUp" :duration="1000">
-    <svg v-if="isSubtitleComplete" class="w-6 h-6 mt-8 text-gray-500 container mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <svg class="w-6 h-6 mt-8 text-gray-500 container mx-auto animate__animated animate__fadeInUp" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
     </svg>
   </transition>
@@ -26,7 +31,6 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import TypeIt from "typeit";
 import * as THREE from "three";
 import CameraControls from "camera-controls";
 
@@ -41,16 +45,20 @@ export default defineComponent({
     };
   },
   mounted() {
-    new TypeIt(".typing", {
-      strings: ["∀Live", "∀Rchive", "∀Social"],
-      cursorChar: "_",
-      speed: 90,
-      lifeLike: true,
-      afterComplete: async (instance: TypeIt) => {
-        this.isTypingComplete = true;
-      },
-    }).go();
     this.initThree();
+    setInterval(function () {
+      const show = document.querySelector("span[data-show]");
+      const next = show?.nextElementSibling || document.querySelector(".typing span:first-child");
+      const up = document.querySelector("span[data-up]");
+
+      if (up) {
+        up.removeAttribute("data-up");
+      }
+      show?.removeAttribute("data-show");
+      show?.setAttribute("data-up", "");
+
+      next?.setAttribute("data-show", "");
+    }, 2500);
   },
   methods: {
     initThree() {
@@ -91,5 +99,36 @@ export default defineComponent({
       animate();
     },
   },
+  beforeDestroy() {
+    clearInterval();
+  },
 });
 </script>
+
+<style scoped>
+.typing span {
+  @apply block absolute bg-no-repeat top-10 md:top-10 xl:top-24 bg-clip-border xl:pb-2.5 text-center md:text-left;
+  background-size: 100% 100%;
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.typing span:nth-child(1) {
+  background-image: linear-gradient(45deg, #fa7671 50%, #f45f7f);
+}
+.typing span:nth-child(2) {
+  background-image: linear-gradient(45deg, #8a7cfb 50%, #633e9c);
+}
+.typing span:nth-child(3) {
+  background-image: linear-gradient(45deg, #18e198 50%, #0ec15d);
+}
+.typing span[data-up] {
+  transform: translateY(-200%);
+  transition: 0.5s transform ease-in-out;
+}
+.typing span[data-show] {
+  transform: translateY(-100%);
+  transition: 0.5s transform ease-in-out;
+}
+</style>
