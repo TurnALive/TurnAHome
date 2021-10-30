e3b2ce01d53706ac94af9a3eced557c71c4f55be
xslingcn <xslings@163.com>
2021-10-31 01:25:45 +0800

    refactor: remove useless entry file

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="w-screen h-screen" style="background: linear-gradient(to right, #c2e59c, #64b3f4)">
+  <div class="w-full h-screen md:h-full 2xl:h-screen shadow-md" style="background: linear-gradient(to right, #c2e59c, #64b3f4)">
     <NavBar />
-    <Home />
+    <Hero />
   </div>
+  <More />
+  <Community />
+  <Alive />
+  <Archive />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import NavBar from "./components/NavBar.vue";
-import Home from "./pages/Home.vue";
+import Hero from "./pages/Hero.vue";
+import Community from "./pages/Community.vue";
+import Alive from "./pages/Alive.vue";
+import Archive from "./pages/Archive.vue";
+import More from "./pages/More.vue";
 
 export default defineComponent({
   setup() {},
   components: {
     NavBar,
-    Home,
+    Hero,
+    More,
+    Community,
+    Alive,
+    Archive,
   },
 });
 </script>
